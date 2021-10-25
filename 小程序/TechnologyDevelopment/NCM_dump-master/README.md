027f89577f29b7873530d6df935d3ca500207c47
Fengyongming0311 <fengyongming0311@163.com>
2021-10-25 12:40:30 +0800

    2021.10.25 新增forkNCM代码

---
+++ 小程序/TechnologyDevelopment/NCM_dump-master/README.md
@@ -0,0 +1,36 @@
+# NCM格式文件转换 (Python version)
+
+## 简介
+
+本项目 fork 自大佬的移植 python 版本项目[nondanee/ncmdump](https://github.com/nondanee/ncmdump) 在此非常感谢
+
+也非常感谢原 C++ 版本代码大佬的 [anonymous5l/ncmdump ](https://github.com/anonymous5l/ncmdump)项目，感谢源代码作者的 Walkman帮了大忙（大雾）
+
+如原作者描述 python 版本运行相对较慢 但是优势在于安装简单（原C++版本需要自行编译taglib相对操作较多）
+
+另：原python版本使用的是pycrypto库相对较老且在部分一体包中的实现相对较差 已替换使用相对较新兼容性较好的pycryptodomex（pycryptodome的共存版本）
+
+仅供学习交流 请勿传播扩散
+
+## 依赖
+
+- python 3.6 （3.*系列）
+
+```
+pip(3) install pycryptodomex mutagen
+```
+
+## 使用
+
+运行GUI即可打开可视化界面
+```
+python(3) GUI.py
+```
+包含两种处理：`单个ncm文件处理`和`指定文件夹内所有ncm文件批量处理`
+
+两种输入方式：
+
+- `输入`文件（夹）路径
+- `按钮选择`单个文件或文件夹
+
+点击`转换`按钮即可进行转换
---
+++ 小程序/TechnologyDevelopment/NCM_dump-master/dump.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 15 01:05:58 2018
+
+@author: Nzix
+
+@mender: FakeCode
+"""
+
+import binascii
+import struct
+import base64
+import json
+import os
+from Cryptodome.Cipher import AES
+from mutagen import mp3, flac, id3
+
+def dumpfile(file_path):
+    # 预定义key
+    core_key = binascii.a2b_hex("687A4852416D736F356B496E62617857")
+    meta_key = binascii.a2b_hex("2331346C6A6B5F215C5D2630553C2728")
+    unpad = lambda s : s[0:-(s[-1] if type(s[-1]) == int else ord(s[-1]))]
+
+    f = open(file_path,'rb')
+
+    # magic header
+    header = f.read(8)
+    assert binascii.b2a_hex(header) == b'4354454e4644414d'
+
+    # key data
+    f.seek(2, 1)
+    key_length = f.read(4)
+    key_length = struct.unpack('<I', bytes(key_length))[0]
+
+    key_data = bytearray(f.read(key_length))
+    key_data = bytes(bytearray([byte ^ 0x64 for byte in key_data]))
+
+    cryptor = AES.new(core_key, AES.MODE_ECB)
+    key_data = unpad(cryptor.decrypt(key_data))[17:]
+    key_length = len(key_data)
+
+    # key box
+    key_data = bytearray(key_data)
+    key_box = bytearray(range(256))
+    j = 0
+
+    for i in range(256):
+        j = (key_box[i] + j + key_data[i % key_length]) & 0xff
+        key_box[i], key_box[j] = key_box[j], key_box[i]
+
+    # meta data
+    meta_length = f.read(4)
+    meta_length = struct.unpack('<I', bytes(meta_length))[0]
+
+    meta_data = bytearray(f.read(meta_length))
+    meta_data = bytes(bytearray([byte ^ 0x63 for byte in meta_data]))
+    meta_data = base64.b64decode(meta_data[22:])
+
+    cryptor = AES.new(meta_key, AES.MODE_ECB)
+    meta_data = unpad(cryptor.decrypt(meta_data)).decode('utf-8')[6:]
+    meta_data = json.loads(meta_data)
+
+    # crc32
+    crc32 = f.read(4)
+    crc32 = struct.unpack('<I', bytes(crc32))[0]
+
+    # album cover
+    f.seek(5, 1)
+    image_size = f.read(4)
+    image_size = struct.unpack('<I', bytes(image_size))[0]
+    image_data = f.read(image_size)
+
+    author_name = ""
+    # media data
+    if len(meta_data['artist']) == 1:
+        author_name = meta_data['artist'][0][0]
+    else:
+        for i in range(len(meta_data['artist'])):
+            if i == len(meta_data['artist']) - 1:
+                author_name += meta_data['artist'][i][0]
+            else:
+                author_name += meta_data['artist'][i][0]
+                author_name += ','
+    file_name = author_name + ' - ' + meta_data['musicName'] + '.' + meta_data['format']
+    music_path = os.path.join(os.path.split(file_path)[0],file_name)
+    m = open(music_path,'wb')
+
+    while True:
+        chunk = bytearray(f.read(0x8000))
+        chunk_length = len(chunk)
+        if not chunk:
+            break
+
+        for i in range(chunk_length):
+            j = (i + 1) & 0xff
+            chunk[i] ^= key_box[(key_box[j] + key_box[(key_box[j] + j) & 0xff]) & 0xff]
+
+        m.write(chunk)
+
+    m.close()
+    f.close()
+
+    # media tag
+    if meta_data['format'] == 'flac':
+        audio = flac.FLAC(music_path)
+        # audio.delete()
+        image = flac.Picture()
+        image.type = 3
+        image.mime = 'image/jpeg'
+        image.data = image_data
+        audio.clear_pictures()
+        audio.add_picture(image)
+    elif meta_data['format'] == 'mp3':
+        audio = mp3.MP3(music_path)
+        # audio.delete()
+        image = id3.APIC()
+        image.type = 3
+        image.mime = 'image/jpeg'
+        image.data = image_data
+        audio.tags.add(image)
+        audio.save()
+        audio = mp3.EasyMP3(music_path)
+
+    audio['title'] = meta_data['musicName']
+    audio['album'] = meta_data['album']
+    audio['artist'] = '/'.join([artist[0] for artist in meta_data['artist']])
+    audio.save()
+
+
